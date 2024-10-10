9e6daaae2ef0388931af3a30df56ba186b50b029
jieunn <jieun776121@gmail.com>
2024-10-10 23:21:29 +0900

    feat: create gnb views

--- app/(tabs)/four.tsx
+++ app/(tabs)/four.tsx
@@ -3,16 +3,16 @@ import { StyleSheet } from "react-native";
 import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
 
-export default function TabOneScreen() {
+export default function TabFourScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>홈</Text>
+      <Text style={styles.title}>마이</Text>
       <View
         style={styles.separator}
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <EditScreenInfo path="app/(tabs)/four.tsx" />
     </View>
   );
 }

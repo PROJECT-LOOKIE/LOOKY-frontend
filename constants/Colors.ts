276ce881eb07f06817c24c6f39b744d8ea330c67
jieunn <jieun776121@gmail.com>
2024-10-31 00:51:46 +0900

    feat: custom color 적용

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,33 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintColorLight = "#2f95dc";
+const tintColorDark = "#fff";
 
 export default {
   light: {
-    text: '#000',
-    background: '#fff',
+    text: "#000",
+    background: "#fff",
     tint: tintColorLight,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#fff',
-    background: '#000',
+    text: "#fff",
+    background: "#000",
     tint: tintColorDark,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorDark,
   },
+
+  ////////////// project //////////////
+  gray900: "#121212",
+  gray800: "#333333",
+  gray700: "#4D4D4D",
+  gray600: "#666666",
+  gray500: "#808080",
+  gray400: "#9A9A9A",
+  gray300: "#B3B3B3",
+  gray200: "#CCCCCC",
+  gray100: "#E6E6E6",
+  background: "#F4F8F3",
+  white: "#FFFFFF",
+  yellowGreen: "#C6FF6B",
 };

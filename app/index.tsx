315c28b0b4a466fbec43bf21bcae1f337dcabec6
Chaeyeoncho <gcy9293@naver.com>
2024-11-11 03:12:20 +0900

    feat : 로그인 화면 구성

---
+++ app/index.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
+import { Link } from "expo-router";
+import Logo from "../assets/images/logo.svg";
+import KakaoIcon from "../assets/images/kakao.svg";
+import AppleIcon from "../assets/images/apple.svg";
+
+export default function Login() {
+  return (
+    <View style={styles.container}>
+      {/* 로고 */}
+      <View style = {styles.logo}>
+      <Logo width={409} height={112} />
+      </View>
+
+      {/* 카카오 로그인 버튼 */}
+      <TouchableOpacity style={styles.kakaoButton}>
+          <KakaoIcon width={345} height={50} />
+      </TouchableOpacity>
+
+      {/* 애플 로그인 버튼 */}
+      <TouchableOpacity style={styles.appleButton}>
+        <AppleIcon width={345} height={50} />
+      </TouchableOpacity>
+
+      {/* 메인 스크린 이동 버튼
+      <Link href="/home" style={styles.link}>
+        <Text>메인 스크린 이동 버튼</Text>
+      </Link> */}
+    </View>
+  );
+}
+
+// 스타일 정의
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#121212",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  logo: {
+    marginTop: 200,
+  },
+
+  kakaoButton: {
+    marginTop: 215,
+  },
+
+  appleButton: {
+    alignItems: "center",
+    marginTop: 30,
+  },
+});

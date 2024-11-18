8c473db49bebbc07ed38ebf68d47fe5f3b7040c2
Chaeyeoncho <gcy9293@naver.com>
2024-11-19 02:54:22 +0900

    fix : 헤더 숨기기

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -59,6 +59,7 @@ function RootLayoutNav() {
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="login/index" options={{ headerShown: false }} />
         {/* <Stack.Screen name="modal" options={{ presentation: "modal" }} /> */}
       </Stack>
     </ThemeProvider>

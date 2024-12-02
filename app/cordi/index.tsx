9177881bbd700c02306ce29d9c9a7ecd5dd46293
jieunn <jieun776121@gmail.com>
2024-12-02 23:46:38 +0900

    chore: 불필요한 파일 삭제

--- app/cordi/index.tsx
+++ app/cordi/index.tsx
@@ -1,7 +1,6 @@
 import { deleteClothItem } from "@/api/closet";
 import Category from "@/components/Category";
 import ClothGrid from "@/components/closet/ClothGrid";
-import CordiZone from "@/components/closet/CordiZone";
 import Banner from "@/components/codi/Banner";
 import MakeCordi from "@/components/codi/makeCordi";
 import CommonHeader from "@/components/CommonHeader";
